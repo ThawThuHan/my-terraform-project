148f337478595b165081ef210c34eab55f705a29
Thaw Thu Han <thawthuhan9@gmail.com>
2023-08-14 06:21:13 -0400

    modularize terraform code

--- main.tf
+++ main.tf
@@ -10,40 +10,13 @@ resource "aws_vpc" "myapp-vpc" {
   }
 }
 
-resource "aws_subnet" "myapp-subnet-1" {
-  vpc_id = aws_vpc.myapp-vpc.id
-  cidr_block = var.subnet_cidr_block
+module "myapp-subnet" {
+  source = "./modules/subnet"
+  subnet_cidr_block = var.subnet_cidr_block
   availability_zone = var.availability_zone
-  map_public_ip_on_launch = true
-  tags = {
-    Name = "${var.env_prefix}-private-subnet-1"
-  }
-}
-
-# data "aws_vpc" "default-vpc" {
-#   default = true
-# }
-
-resource "aws_internet_gateway" "myapp-igw" {
-  vpc_id = aws_vpc.myapp-vpc.id 
-}
-
-#using existing route table that is created by default
-resource "aws_default_route_table" "main-rtb" {
+  env_prefix = var.env_prefix
+  vpc_id = aws_vpc.myapp-vpc.id
   default_route_table_id = aws_vpc.myapp-vpc.default_route_table_id
-
-  route {
-    cidr_block = "0.0.0.0/0"
-    gateway_id = aws_internet_gateway.myapp-igw.id
-  }
-  tags = {
-    Name = "${var.env_prefix}-main-rtb"
-  }
-}
-
-resource "aws_route_table_association" "route-subnet-association" {
-  subnet_id = aws_subnet.myapp-subnet-1.id
-  route_table_id = aws_default_route_table.main-rtb.id
 }
 
 # resource "aws_route_table" "myapp-route-table" {
